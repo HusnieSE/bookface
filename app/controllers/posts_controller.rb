83d9154da5fe54003877d3e0642aaec273670626
husniese <husnie.edres@shoresuite.com>
2018-05-12 12:37:33 +0800

    can add with user_id and display user email add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,9 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new
-    # $stderr.puts ("sdsadaaaaaa #{params[:contents]}")
+    $stderr.puts ("sdsadaaaaaa #{current_user[:id].inspect}")
     post.contents = params[:contents]
+    post.user_id = current_user[:id]
     post.save
     @posts = Post.all.order("created_at DESC")
     $stderr.puts ("posts #{@posts.inspect}")
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     validates :contents, presence: true, allow_blank: false
+    belongs_to :user
 end
