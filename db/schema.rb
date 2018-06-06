7a39f3611a44d2ad8ea20a620a1425288f72f93b
husniese <husnie.edres@shoresuite.com>
2018-06-06 21:49:22 +0800

    tryyyyyy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606134513) do
+ActiveRecord::Schema.define(version: 20180606134844) do
 
   create_table "comments", force: :cascade do |t|
     t.text "contents", null: false
